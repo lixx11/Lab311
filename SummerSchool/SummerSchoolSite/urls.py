0649f04d18f39669058f9b4de308a683295e312f
lixx11 <lxx2011011580@gmail.com>
2018-02-20 21:00:49 +0800

    add media support for development

--- SummerSchool/SummerSchoolSite/urls.py
+++ SummerSchool/SummerSchoolSite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from camper import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path('accounts/', include('registration.backends.default.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
