abdf7e5bb4c08c2c95b925f25f188e83b0e0e8d8
lixx11 <lxx2011011580@gmail.com>
2018-05-02 16:30:31 +0800

    fix file permission bug

--- SummerSchool/summer_school/settings.py
+++ SummerSchool/summer_school/settings.py
@@ -47,6 +47,8 @@ if not DEBUG:
 else:
     ALLOWED_HOSTS = ['*', ]
 
+FILE_UPLOAD_PERMISSIONS = 0o644
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
